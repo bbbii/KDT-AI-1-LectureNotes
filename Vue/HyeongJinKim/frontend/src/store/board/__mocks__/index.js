f9bb88be1e9454a1e4b489122fd171f1437ede11
bbbii <hyeongjin1326@gmail.com>
2023-07-11 16:11:16 +0900

    feat: [김형진] Vue에서 수정 요청 검증 [VUE-UNIT-JESTx]

--- Vue/HyeongJinKim/frontend/src/store/board/__mocks__/index.js
+++ Vue/HyeongJinKim/frontend/src/store/board/__mocks__/index.js
@@ -76,6 +76,19 @@ export const actions = {
       return true;
     }
   }),
+
+  requestBoardModifyToSpring: jest.fn((payload) => {
+    const { title, content, boardId } = payload;
+
+    let board = getters.getBoard();
+
+    if (board[0].boardId === boardId) {
+      board[0].title = title;
+      board[0].content = content;
+    }
+
+    return board;
+  }),
 };
 
 export const state = {};
